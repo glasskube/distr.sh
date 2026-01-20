5f22b778a1460cbb604af23a6ddde9d6bbfd8f4e
Philip Miglinci <pmig@glasskube.com>
2026-01-20 14:43:33 +0100

    chore: fix Header linting
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,28 +1,23 @@
 export type NavigationLink = {
   title: string;
   value: string;
-  transfer: boolean;
 };
 
 export const links: NavigationLink[] = [
   {
     title: 'Docs',
     value: '/docs/',
-    transfer: true,
   },
   {
     title: 'Pricing',
     value: '/pricing/',
-    transfer: true,
   },
   {
     title: 'Blog',
     value: '/blog/',
-    transfer: false,
   },
   {
     title: 'Case Studies',
     value: '/case-studies/',
-    transfer: true,
   },
 ];
