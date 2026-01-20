bdda39a158862eca2699a3a03d0dc63f956d2bca
Philip Miglinci <pmig@glasskube.com>
2026-01-20 16:22:20 +0100

    feat: improve Distr Header (#243)
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

---
+++ src/data/navigation.ts
@@ -0,0 +1,38 @@
+export type NavigationLink = {
+  title: string;
+  value: string;
+  children?: NavigationLink[];
+};
+
+export const links: NavigationLink[] = [
+  {
+    title: 'Docs',
+    value: '/docs/',
+  },
+  {
+    title: 'Pricing',
+    value: '/pricing/',
+  },
+  {
+    title: 'Resources',
+    value: 'javascript:void(0)',
+    children: [
+      {
+        title: 'Blog',
+        value: '/blog/',
+      },
+      {
+        title: 'Case Studies',
+        value: '/case-studies/',
+      },
+      {
+        title: 'Glossary',
+        value: '/glossary/',
+      },
+      {
+        title: 'White Paper',
+        value: '/white-paper/building-blocks/',
+      },
+    ],
+  },
+];
